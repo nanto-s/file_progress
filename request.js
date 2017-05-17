5893d4413e44dca73559439a1630c90d7c62c706
nanto-s <nata.serebro@gmail.com>
2017-05-17 16:08:45 +0300

    html code upload one time (delete fileStream.pipe(res); in .on('open', () => {...

--- request.js
+++ request.js
@@ -7,6 +7,7 @@ let url = require('url');
 let fs = require('fs');
 let path = require('path');
 let config = require('./config/param');
+//let config = require('config');
 const mime = require('mime');
 
 
@@ -14,7 +15,6 @@ module.exports = ((req, res) => {
 
     let pathname = decodeURI(url.parse(req.url).pathname);
     let pathFiles = path.normalize(path.join(config.FILES, pathname.slice(1)));
-
     if (req.method === 'GET') {
         if (pathname === '/') {
             sendFile(`${config.ROOT}/index.html`, res);
@@ -23,30 +23,33 @@ module.exports = ((req, res) => {
         }
     }
     if (req.method === 'POST') {
-        console.log('POST pathname '+pathname);
+        console.log('POST pathname ' + pathname);
         let nf = fs.createWriteStream(pathFiles);
         req.pipe(nf);
-        req.on('end', () => {res.end ('Uploaded!');});
+        req.on('end', () => {
+            res.end('Uploaded!');
+        });
     }
 });
 
 
 function sendFile(fileName, res) {
     let fileStream = fs.createReadStream(fileName);
-      fileStream.pipe(res);
+    fileStream.pipe(res);
 
     fileStream
-    .on('open', () => {
-            res.setHeader('Content-Type', mime.lookup(fileName));
-            console.log( '***'+mime.lookup(fileName));
-      fileStream.pipe(res);
-        })
 
-        .on('error', function() {
+        .on('error', function () {
             res.statusCode = 500;
             res.end("Server error");
         })
-        .on('close', function() {
+
+        .on('open', () => {
+            res.setHeader('Content-Type', mime.lookup(fileName));
+            console.log('***' + mime.lookup(fileName));
+        })
+
+        .on('close', function () {
             fileStream.destroy();
         });
 
